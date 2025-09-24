77debb5034bce3eb9237d386fbee63c7c3cd108f
Ruben <r.ternero.molina@iescristobaldemonroy.es>
2025-09-24 16:59:28 +0200

    Añadidos variables

---
+++ variablesejercicio.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$numero1 = 3;
+$numero2 = 4;
+$suma = $numero1 + $numero2;
+
+echo '<p> La suma total es ' . $numero1 + $numero2 . '</p>';
+echo '<p> La suma total es ' . $suma . '</p>';
+
+
+    ?>
+    
+</body>
+</html>
---
+++ variablestipos.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+$x= 10;
+$y = "Esto es un texto";
+$z = false;
+$q = 2.25;
+
+var_dump($x);
+var_dump($y);
+var_dump($z) ;
+var_dump($q); 
+
+
+?>
