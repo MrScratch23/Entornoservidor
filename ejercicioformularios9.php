dc413fa44bed4b236977ddac154efaf339198f35
Ruben <r.ternero.molina@iescristobaldemonroy.es>
2025-10-10 21:12:36 +0200

    modified:   ejercicioformularios9.php

--- ejercicioformularios9.php
+++ ejercicioformularios9.php
@@ -73,7 +73,7 @@ if ($_SERVER['REQUEST_METHOD'] === "GET" && isset($_GET['generar'])) {
 <head>
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <title>P.Lluyot</title>
+    <title>Ejercicio 9</title>
     <link rel='stylesheet' href='https://cdn.simplecss.org/simple.min.css'>
 </head>
 
