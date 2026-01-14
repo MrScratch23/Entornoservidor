ab999bef88297cdb5940c4d63aee424aeb6d3f59
Ruben <r.ternero.molina@iescristobaldemonroy.es>
2026-01-14 21:49:32 +0100

    commit

--- POOClasesYObjetos/EX32526/app/controllers/LoginController.php
+++ POOClasesYObjetos/EX32526/app/controllers/LoginController.php
@@ -64,25 +64,14 @@ class LoginController extends Controller {
     }
 
     public static function cerrarSesion() {
-        if (session_status() === PHP_SESSION_NONE) {
-            session_start();
-        }
+       
 
-        // vaciar sesión + cookie
-        $_SESSION = [];
-        if (ini_get('session.use_cookies')) {
-            $params = session_get_cookie_params();
-            setcookie(session_name(), '', time() - 42000,
-                $params['path'], $params['domain'],
-                $params['secure'], $params['httponly']
-            );
-        }
         session_unset();
         session_destroy();
+        header("Location: login", true, 302);
+        exit();
 
-        // redirigir usando BASE_URL (ruta pública)
-        header('Location: ' . BASE_URL . '/login');
-        exit;
+       
     }
 }
 ?>
